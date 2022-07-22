52df63572c09d336df4bf610c933ded1a652b0d9
Ben <105577567+benjbright@users.noreply.github.com>
2022-07-22 13:17:22 +0100

    Update LotteryEntrance.jsx

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -17,7 +17,11 @@ export default function LotteryEntrance() {
 
     const dispatch = useNotification()
 
-    const { runContractFunction: enterRaffle } = useWeb3Contract({
+    const {
+        runContractFunction: enterRaffle,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
@@ -82,19 +86,25 @@ export default function LotteryEntrance() {
     }
 
     return (
-        <>
+        <div className="p-5">
             <h1>Hi from lottery entrance!</h1>
             {raffleAddress ? (
                 <div>
                     <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         onClick={async function () {
                             await enterRaffle({
                                 onSuccess: handleSuccess,
                                 onError: (error) => console.log(error),
                             })
                         }}
+                        disabled={isLoading || isFetching}
                     >
-                        Enter Raffle
+                        {isLoading || isFetching ? (
+                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                        ) : (
+                            <div>Enter Raffle</div>
+                        )}
                     </button>
                     <p>
                         Entrance Fee:{" "}
@@ -106,6 +116,6 @@ export default function LotteryEntrance() {
             ) : (
                 <div>No Raffle Address Detected</div>
             )}
-        </>
+        </div>
     )
 }
